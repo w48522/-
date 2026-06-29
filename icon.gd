a290d2e9e37d0bb019c28bc3b9a99d93838a8b9a
main485 <1807775907@qq.com>
2026-06-29 15:00:59 +0800

    Revert "test 3"
    This reverts commit 60deceb0664e07862cbbb5c5f993cb45b11d8542.

--- icon.gd
+++ icon.gd
@@ -4,9 +4,3 @@ func _ready() -> void:
 	print("okokok,this is the Frist test!")
 	print("fuck you")
 	print("yesyesyes")
-	print("yesyesyes")
-	print("yesyesyes")
-	print("yesyesyes")
-	print("yesyesyes")
-
-	print("this is a branch")
